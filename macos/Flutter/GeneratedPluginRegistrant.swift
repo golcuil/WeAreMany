b8878ea3e5d39f1ad889b0b5fbf6cfb90c465b6a
Berkay Gölcü <golcuil@mef.edu.tr>
2026-01-22 12:12:19 +0300

    Fix macOS GeneratedPluginRegistrant whitespace (T-025)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import shared_preferences_foundation
 
-
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
